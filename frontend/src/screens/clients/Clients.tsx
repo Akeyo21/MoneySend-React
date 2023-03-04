dffbd35a7362f75775e38f571dad087d6541fc00
Akeyo21 <vickyakeke@gmail.com>
2023-03-04 12:29:48 +0300

    Using flatlist to display a lsit of clients

--- frontend/src/screens/clients/Clients.tsx
+++ frontend/src/screens/clients/Clients.tsx
@@ -1,33 +1,85 @@
 import {useQuery} from '@apollo/client';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import {styles} from './Clients.style';
 
 import {GET_CLIENTS} from '../../../App.query';
 import ClientCard from './components/ClientCard';
+import {clientType} from './Clients.type';
 
 function Clients() {
-  const {loading, error, data} = useQuery(GET_CLIENTS);
-  if (loading) {
-    return <Text>Loading...</Text>;
-  }
-  if (error) {
-    console.log('ERROR', error);
-    return <Text>Error : {error.message}</Text>;
-  }
+  // const {loading, error, data} = useQuery(GET_CLIENTS);
+  // if (loading) {
+  //   return <Text>Loading...</Text>;
+  // }
+  // if (error) {
+  //   console.log('ERROR', error);
+  //   return <Text>Error : {error.message}</Text>;
+  // }
 
-  console.log('CLIENTS', data.clients);
+  // console.log('CLIENTS', data.clients);
+  const testData: clientType[] = [
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+    {
+      name: 'Michelle Amy',
+      phoneNumber: '0721345678',
+      totalAmountBorrowed: 10000,
+      totalInterestDue: 1000,
+    },
+  ];
   return (
     <View style={styles.container}>
-      <ClientCard />
-      <ClientCard />
-
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
-      <ClientCard />
+      <FlatList
+        data={testData}
+        renderItem={({item}) => <ClientCard {...item} />}
+      />
     </View>
   );
 }
