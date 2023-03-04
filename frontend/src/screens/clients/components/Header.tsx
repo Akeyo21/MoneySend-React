a9a7720d724419760bf6cf105e1033910c00edb7
Akeyo21 <vickyakeke@gmail.com>
2023-03-04 14:08:53 +0300

    Adding styles to the header

--- frontend/src/screens/clients/components/Header.tsx
+++ frontend/src/screens/clients/components/Header.tsx
@@ -6,11 +6,21 @@ type props = {
 };
 const Header = ({headerText}: props) => {
   return (
-    <View>
+    <View
+      style={{
+        height: 50,
+        elevation: 9,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 1000,
+      }}>
       <Text
         style={{
           textTransform: 'capitalize',
           color: 'black',
+          fontSize: 40,
+          fontWeight: '600',
         }}>
         {headerText}
       </Text>
