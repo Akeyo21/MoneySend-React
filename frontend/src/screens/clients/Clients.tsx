55430fcfe200aeb5b52b5dae9cb52a67b10dd7a1
Akeyo21 <vickyakeke@gmail.com>
2023-03-04 14:08:17 +0300

    Adding header and add client button

--- frontend/src/screens/clients/Clients.tsx
+++ frontend/src/screens/clients/Clients.tsx
@@ -1,35 +1,36 @@
-import {useQuery} from '@apollo/client';
 import React from 'react';
-import {FlatList, View} from 'react-native';
-
-import {GET_CLIENTS} from '../../../App.query';
-import ClientCard from './components/ClientCard';
+import {FlatList, Text, TouchableOpacity, View} from 'react-native';
 import {testData} from './Clients.test-data';
+import ClientCard from './components/ClientCard';
 import Header from './components/Header';
 
 function Clients() {
-  // const {loading, error, data} = useQuery(GET_CLIENTS);
-  // if (loading) {
-  //   return <Text>Loading...</Text>;
-  // }
-  // if (error) {
-  //   console.log('ERROR', error);
-  //   return <Text>Error : {error.message}</Text>;
-  // }
-
-  // console.log('CLIENTS', data.clients);
-
   return (
     <View
       style={{
         backgroundColor: 'white',
         height: '100%',
       }}>
+      <Header headerText="Clients" />
       <FlatList
         data={testData}
-        ListHeaderComponent={<Header headerText="Clients" />}
         renderItem={({item}) => <ClientCard {...item} />}
       />
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+          style={{
+            width: '50%',
+            backgroundColor: 'green',
+            position: 'absolute',
+            bottom: 20,
+            borderRadius: 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 50,
+          }}>
+          <Text style={{color: 'white', fontSize: 20}}>Add Client</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
