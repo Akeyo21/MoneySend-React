c63f1fc6f2d0c1ebd5660430cef23305d4438ce3
Akeyo21 <vickyakeke@gmail.com>
2023-08-17 21:27:51 +0300

    Add user icon

--- frontend/src/screens/clients/components/ClientCard.tsx
+++ frontend/src/screens/clients/components/ClientCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleProp, Text, TextStyle, View} from 'react-native';
 import {formatMoneyText} from '../../../utils/moneyFormatter';
 import {clientType} from '../Clients.type';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const text: StyleProp<TextStyle> = {
   color: 'black',
@@ -21,23 +22,31 @@ const ClientCard = ({
         padding: '5%',
         margin: '5%',
         flexDirection: 'row',
-        justifyContent: 'space-between',
         borderRadius: 10,
         elevation: 9,
         backgroundColor: 'white',
         shadowColor: 'green',
-        shadowOpacity: 0.1,
+        shadowOpacity: 0.5,
       }}>
-      <View>
-        <Text style={{...text, fontWeight: '500'}}>{name}</Text>
-        <Text style={{...text, fontWeight: '300'}}>{phoneNumber}</Text>
-      </View>
+      <Icon name="account-circle" size={40} color="#026600" />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          flex: 1,
+          marginLeft: 10,
+        }}>
+        <View>
+          <Text style={{...text, fontWeight: '500'}}>{name}</Text>
+          <Text style={{...text, fontWeight: '300'}}>{phoneNumber}</Text>
+        </View>
 
-      <View>
-        <Text style={amountTextStyle}>
-          {formatMoneyText(outstandingBalance)}
-        </Text>
-        <Text style={amountTextStyle}>{formatMoneyText(interest)}</Text>
+        <View>
+          <Text style={amountTextStyle}>
+            {formatMoneyText(outstandingBalance)}
+          </Text>
+          <Text style={amountTextStyle}>{formatMoneyText(interest)}</Text>
+        </View>
       </View>
     </View>
   );
