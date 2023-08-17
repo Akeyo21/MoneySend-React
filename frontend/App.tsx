d59f32309ec7b103b4911efa9129b510955aa42c
Akeyo21 <vickyakeke@gmail.com>
2023-08-17 21:18:02 +0300

    Add tab view

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -8,6 +8,10 @@
 import React from 'react';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import Clients from './src/screens/clients/Clients';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {TouchableOpacity} from 'react-native';
 
 function App(): JSX.Element {
   const client = new ApolloClient({
@@ -15,10 +19,38 @@ function App(): JSX.Element {
     cache: new InMemoryCache(),
   });
 
+  const Tab = createBottomTabNavigator();
+
   return (
-    <ApolloProvider client={client}>
-      <Clients />
-    </ApolloProvider>
+    <NavigationContainer>
+      <ApolloProvider client={client}>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: '#026600',
+            tabBarInactiveTintColor: '#4C574C',
+            tabBarButton: props => <TouchableOpacity {...props} />,
+            headerTitleStyle: {
+              fontSize: 30,
+              fontWeight: '600',
+            },
+          }}>
+          <Tab.Screen
+            name="Clients"
+            component={Clients}
+            options={{
+              tabBarIcon: props => <Icon name="groups" {...props} />,
+            }}
+          />
+          <Tab.Screen
+            name="Client Records"
+            component={Clients}
+            options={{
+              tabBarIcon: props => <Icon name="view-list" {...props} />,
+            }}
+          />
+        </Tab.Navigator>
+      </ApolloProvider>
+    </NavigationContainer>
   );
 }
 
