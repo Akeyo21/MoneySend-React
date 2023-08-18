8c42ec51fc457c3315a27c38793cffa5748b8bae
Akeyo21 <vickyakeke@gmail.com>
2023-08-18 21:05:54 +0300

    Add style constants

---
+++ frontend/src/screens/style.constants.ts
@@ -0,0 +1,12 @@
+export const headerTitleStyle: any = {
+  fontSize: 23,
+  fontWeight: '600',
+};
+
+export const screenOptions: any = {
+  headerTitleAlign: 'center',
+  headerTitleStyle: {...headerTitleStyle},
+  headerBackTitleStyle: {
+    fontSize: 13,
+  },
+};
