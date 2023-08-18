ea0b19457e2deeecaddf72cd5d22a01d0733d38a
Akeyo21 <vickyakeke@gmail.com>
2023-08-18 20:36:06 +0300

    Add the add client record button to the client records page

--- frontend/src/screens/client-records/ClientRecords.tsx
+++ frontend/src/screens/client-records/ClientRecords.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import AddButton from '../../components/AddButton';
+import {addClientRecordScreen} from './ClientRecords.screens';
 
-const ClientRecords = () => {
+const ClientRecords = ({navigation}: any) => {
   return (
-    <View style={{justifyContent: 'center', alignItems: 'center'}}>
-      <Text>CLIENT RECORDS HERE</Text>
+    <View>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}>
+        <Text>CLIENT RECORDS HERE</Text>
+      </View>
+      <AddButton
+        buttonText="Add Client Record"
+        onPress={() => {
+          navigation.navigate(addClientRecordScreen);
+        }}
+      />
     </View>
   );
 };
