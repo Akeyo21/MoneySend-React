bb528a1336374c8b5b4a8f62ef2f4bbb318d8953
Akeyo21 <vickyakeke@gmail.com>
2023-08-18 20:35:40 +0300

    Add the add client record component to the client record screens

--- frontend/src/screens/client-records/ClientRecords.screens.tsx
+++ frontend/src/screens/client-records/ClientRecords.screens.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ClientRecords from './ClientRecords';
+import AddClientRecord from './ClientRecords.add';
 
+export const addClientRecordScreen = 'Add Client Record';
 const ClientRecordsStackScreen = () => {
   const ClientRecordsStack = createNativeStackNavigator();
   return (
@@ -10,6 +12,10 @@ const ClientRecordsStackScreen = () => {
       <ClientRecordsStack.Screen
         name="Client Records"
         component={ClientRecords}
+      />
+      <ClientRecordsStack.Screen
+        name={addClientRecordScreen}
+        component={AddClientRecord}
       />
     </ClientRecordsStack.Navigator>
   );
