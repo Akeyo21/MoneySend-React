badfe80c0ca1ac7a5890f43488c1647493196878
Akeyo21 <vickyakeke@gmail.com>
2023-08-18 21:07:43 +0300

    Add screen options to the navigators

--- frontend/src/screens/client-records/ClientRecords.screens.tsx
+++ frontend/src/screens/client-records/ClientRecords.screens.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ClientRecords from './ClientRecords';
 import AddClientRecord from './ClientRecords.add';
+import {screenOptions} from '../style.constants';
 
 export const addClientRecordScreen = 'Add Client Record';
 const ClientRecordsStackScreen = () => {
   const ClientRecordsStack = createNativeStackNavigator();
   return (
-    <ClientRecordsStack.Navigator>
+    <ClientRecordsStack.Navigator
+      screenOptions={{...screenOptions, headerBackTitle: 'Records'}}>
       <ClientRecordsStack.Screen
         name="Client Records"
         component={ClientRecords}
--- frontend/src/screens/clients/Clients.screens.tsx
+++ frontend/src/screens/clients/Clients.screens.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Clients from './Clients';
 import AddClient from './Clients.add-client';
+import {screenOptions} from '../style.constants';
 
 export const AddClientScreen = 'Add Client';
 const ClientsStackScreen = () => {
   const ClientsStack = createNativeStackNavigator();
   return (
-    <ClientsStack.Navigator>
+    <ClientsStack.Navigator screenOptions={screenOptions}>
       <ClientsStack.Screen name="Clients" component={Clients} />
       <ClientsStack.Screen name={AddClientScreen} component={AddClient} />
     </ClientsStack.Navigator>
