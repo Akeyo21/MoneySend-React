426ce8a76fd5a1ebfbf5fca0ee7bd8f7d5a678f8
Akeyo21 <vickyakeke@gmail.com>
2023-08-18 20:25:11 +0300

    Update icon import

--- frontend/src/components/AddButton.tsx
+++ frontend/src/components/AddButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, TouchableOpacity, Text} from 'react-native';
-import {Icon} from 'react-native-vector-icons/Icon';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {primary} from '../color.constants';
 
 type props = {
