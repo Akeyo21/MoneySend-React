8a9c0a36930989bd16739c7284366c4d00f54e5d
Akeyo21 <vickyakeke@gmail.com>
2023-08-18 20:35:20 +0300

    Component that adds a client record

---
+++ frontend/src/screens/client-records/ClientRecords.add.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AddClientRecord = () => {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <Text>ADD CLIENT RECORD HERE</Text>
+    </View>
+  );
+};
+
+export default AddClientRecord;
