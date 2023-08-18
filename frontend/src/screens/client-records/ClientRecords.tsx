12594d723755ad75bdacb6d5b13952401329de95
Akeyo21 <vickyakeke@gmail.com>
2023-08-18 20:25:29 +0300

    Add new components for adding a client

---
+++ frontend/src/screens/client-records/ClientRecords.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ClientRecords = () => {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <Text>CLIENT RECORDS HERE</Text>
+    </View>
+  );
+};
+
+export default ClientRecords;
---
+++ frontend/src/screens/clients/Clients.add-client.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AddClient = () => {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <Text>ADD CLIENT</Text>
+    </View>
+  );
+};
+
+export default AddClient;
