4083b06e5b47b75c60b828f703ff586997636641
Akeyo21 <vickyakeke@gmail.com>
2023-08-21 21:05:51 +0300

    Add date view to the add client form

--- frontend/src/screens/clients/Clients.add-client.tsx
+++ frontend/src/screens/clients/Clients.add-client.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {primary} from '../../color.constants';
 
 const AddClient = () => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
   return (
-    <View style={{justifyContent: 'center', alignItems: 'center'}}>
-      <Text>ADD CLIENT</Text>
+    <View style={{padding: 30}}>
+      <View>
+        <Text style={{fontWeight: '700'}}>Date</Text>
+
+        <View
+          style={{
+            backgroundColor: 'white',
+            padding: 10,
+            borderRadius: 5,
+            marginTop: 5,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <Text style={{fontSize: 16}}>
+            {date.getDay()}/ {date.getMonth()}/{date.getFullYear()}
+          </Text>
+          <Icon
+            name="calendar-today"
+            color={primary}
+            size={20}
+            onPress={() => {
+              setOpen(true);
+            }}
+          />
+        </View>
+      </View>
+      <DatePicker
+        date={date}
+        onDateChange={setDate}
+        mode="date"
+        modal
+        open={open}
+        onConfirm={(pickedDate: any) => {
+          setOpen(false);
+          setDate(pickedDate);
+        }}
+        onCancel={() => {
+          setOpen(false);
+        }}
+      />
     </View>
   );
 };
