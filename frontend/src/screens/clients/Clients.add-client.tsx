20269d33577cefdcd9fce4036f9d98581b2e3048
Akeyo21 <vickyakeke@gmail.com>
2023-09-06 20:55:05 +0300

    Add name text field

--- frontend/src/screens/clients/Clients.add-client.tsx
+++ frontend/src/screens/clients/Clients.add-client.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import {View} from 'react-native';
-import DateChange from './Clients.add-clitent.date';
+import React, {useState} from 'react';
+import {TextInput, View, Text} from 'react-native';
 
 const AddClient = () => {
+  const [name, setName] = useState('');
+
   return (
     <View style={{padding: 30}}>
-      <DateChange />
+      <Text style={{fontWeight: '600', fontSize: 16}}>Name</Text>
+      <TextInput
+        style={{
+          height: 50,
+          borderWidth: 1,
+          padding: 10,
+          backgroundColor: 'white',
+          borderRadius: 5,
+          marginTop: 5,
+        }}
+        autoFocus
+        inputMode="text"
+        value={name}
+        onChangeText={setName}
+        autoCapitalize="words"
+        placeholder="First and Last Name"
+      />
     </View>
   );
 };
