86bbe9887d9ffa84c9b63167000165efb536aef8
Akeyo21 <vickyakeke@gmail.com>
2023-10-04 21:25:47 +0300

    Add CTA button on add client screen

--- frontend/src/screens/clients/Clients.add-client.tsx
+++ frontend/src/screens/clients/Clients.add-client.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useRef} from 'react';
 import {TextInput, View, Text} from 'react-native';
 import {primary} from '../../color.constants';
+import CTAButton from '../../components/CTAButton';
 
 const AddClient = () => {
   const [name, setName] = useState('');
@@ -11,81 +12,90 @@ const AddClient = () => {
   const [refereeInputIsFocused, setRefereeInputIsFocused] = useState(false);
 
   return (
-    <View style={{padding: 30}}>
-      <Text style={{fontWeight: '600', fontSize: 16}}>Name</Text>
-      <TextInput
-        style={{
-          height: 50,
-          borderWidth: 1,
-          padding: 10,
-          backgroundColor: 'white',
-          borderColor: nameInputIsFocused ? primary : 'grey',
-          borderRadius: 5,
-          marginTop: 5,
-        }}
-        onFocus={() => {
-          setNameIsFocused(true);
-        }}
-        onBlur={() => {
-          setNameIsFocused(false);
-        }}
-        autoFocus
-        inputMode="text"
-        value={name}
-        onChangeText={setName}
-        autoCapitalize="words"
-        placeholder="First and Last Name"
-      />
-
-      <View style={{marginTop: 20}}>
-        <Text style={{fontWeight: '600', fontSize: 16}}>Phone Number</Text>
+    <View
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        flex: 1,
+      }}>
+      <View style={{padding: 30}}>
+        <Text style={{fontWeight: '600', fontSize: 16}}>Name</Text>
         <TextInput
           style={{
             height: 50,
             borderWidth: 1,
             padding: 10,
             backgroundColor: 'white',
-            borderColor: numberInputIsFocused ? primary : 'grey',
+            borderColor: nameInputIsFocused ? primary : 'grey',
             borderRadius: 5,
             marginTop: 5,
           }}
           onFocus={() => {
-            setNumberIsFocused(true);
+            setNameIsFocused(true);
           }}
           onBlur={() => {
-            setNumberIsFocused(false);
+            setNameIsFocused(false);
           }}
-          inputMode="tel"
-          value={phone}
-          onChangeText={setPhone}
-          placeholder="0712345678"
-        />
-      </View>
-
-      <View style={{marginTop: 20}}>
-        <Text style={{fontWeight: '600', fontSize: 16}}>Referee</Text>
-        <TextInput
-          style={{
-            height: 50,
-            borderWidth: 1,
-            padding: 10,
-            backgroundColor: 'white',
-            borderColor: refereeInputIsFocused ? primary : 'grey',
-            borderRadius: 5,
-            marginTop: 5,
-          }}
-          onFocus={() => {
-            setRefereeInputIsFocused(true);
-          }}
-          onBlur={() => {
-            setRefereeInputIsFocused(false);
-          }}
+          autoFocus
           inputMode="text"
-          value={referee}
-          onChangeText={setReferee}
+          value={name}
+          onChangeText={setName}
+          autoCapitalize="words"
           placeholder="First and Last Name"
         />
+
+        <View style={{marginTop: 20}}>
+          <Text style={{fontWeight: '600', fontSize: 16}}>Phone Number</Text>
+          <TextInput
+            style={{
+              height: 50,
+              borderWidth: 1,
+              padding: 10,
+              backgroundColor: 'white',
+              borderColor: numberInputIsFocused ? primary : 'grey',
+              borderRadius: 5,
+              marginTop: 5,
+            }}
+            onFocus={() => {
+              setNumberIsFocused(true);
+            }}
+            onBlur={() => {
+              setNumberIsFocused(false);
+            }}
+            inputMode="tel"
+            value={phone}
+            onChangeText={setPhone}
+            placeholder="0712345678"
+          />
+        </View>
+
+        <View style={{marginTop: 20}}>
+          <Text style={{fontWeight: '600', fontSize: 16}}>Referee</Text>
+          <TextInput
+            style={{
+              height: 50,
+              borderWidth: 1,
+              padding: 10,
+              backgroundColor: 'white',
+              borderColor: refereeInputIsFocused ? primary : 'grey',
+              borderRadius: 5,
+              marginTop: 5,
+            }}
+            onFocus={() => {
+              setRefereeInputIsFocused(true);
+            }}
+            onBlur={() => {
+              setRefereeInputIsFocused(false);
+            }}
+            inputMode="text"
+            value={referee}
+            onChangeText={setReferee}
+            placeholder="First and Last Name"
+          />
+        </View>
       </View>
+
+      <CTAButton buttonText="Add Client" onPress={() => {}} iconName="add" />
     </View>
   );
 };
